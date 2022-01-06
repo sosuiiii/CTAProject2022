d2d74b4f8e6ca7cfc1b9c0e13d94631a2d4f874b
Soushi Tanaka <wntsssbbsnws@yahoo.co.jp>
2022-01-06 20:27:15 +0900

    Add mockolo

--- Scripts/before-compile.sh
+++ Scripts/before-compile.sh
@@ -5,3 +5,10 @@ swiftlint autocorrect --format
 else
 echo “warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint”
 fi
+
+if which mint >/dev/null; then
+  rm -f $SRCROOT/Generated/MockResults.swift
+  mint run mockolo mockolo --sourcedirs $SRCROOT/CTAProject --destination $SRCROOT/Generated/MockResults.swift
+else
+  echo "warning: Mint not installed, download from https://github.com/yonaskolb/Mint"
+fi
