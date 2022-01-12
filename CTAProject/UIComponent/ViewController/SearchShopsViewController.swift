62fc68af0acfce473d3effc2c95ade1d4aba7d9f
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-01-12 15:59:05 +0900

    Add SearchShopsViewController

---
+++ CTAProject/UIComponent/ViewController/SearchShopsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchShopsViewController.swift
+//  CTAProject
+//
+//  Created by Tomoya Tanaka on 2022/01/12.
+//
+
+import UIKit
+
+class SearchShopsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.navigationItem.title = "グルスポ"
+    }
+    
+}
