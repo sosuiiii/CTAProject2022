e5227abad0f47e1b7afd7c34080e9c385bb8b036
Mayachiu <mayachiu5g@gmail.com>
2022-01-13 17:02:45 +0900

    Add SearchShopViewController.swift and SearchShopViewController.xib Delete ViewController.swift Fix SceneDelegate.swift

---
+++ CTAProject/UIComponent/ViewController/SearchShopViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SearchShopViewController.swift
+//  CTAProject
+//
+//  Created by 内山和輝 on 2022/01/13.
+//
+
+import UIKit
+
+class SearchShopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
